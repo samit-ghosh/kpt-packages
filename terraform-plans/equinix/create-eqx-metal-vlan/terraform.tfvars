48775024e35453bfe38d6a9468bc645401044940
Tamilselvanm <tamilselvan@aarnanetworks.com>
2024-02-22 10:28:49 +0530

    removed the clinet secret variable

--- terraform-plans/equinix/create-eqx-metal-vlan/terraform.tfvars
+++ terraform-plans/equinix/create-eqx-metal-vlan/terraform.tfvars
@@ -1,5 +1,3 @@
-eqx_metal_client_id         ="USER_INPUT"
-eqx_metal_client_secret     ="USER_INPUT"
 eqx_metal_project_id        ="USER_INPUT"
 eqx_metal_metro_code        ="USER_INPUT"
 eqx_metal_vlan              =1067
--- terraform-plans/equinix/create-eqx-metal-vlan/variables.tf
+++ terraform-plans/equinix/create-eqx-metal-vlan/variables.tf
@@ -1,5 +1,3 @@
-variable "eqx_metal_client_id"             {}
-variable "eqx_metal_client_secret"         {}
 variable "eqx_metal_project_id"            {}
 variable "eqx_metal_metro_code"            {}
 variable "eqx_metal_vlan"                  {}
