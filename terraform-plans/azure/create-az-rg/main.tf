398f5239c5d034f17c71a584f98ad5cbb27b9b37
Tamilselvan Mani <tamilselvan@aarnanetworks.com>
2024-02-22 13:03:36 +0000

    Update main.tf

--- terraform-plans/azure/create-az-rg/main.tf
+++ terraform-plans/azure/create-az-rg/main.tf
@@ -12,10 +12,6 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-provider "azurerm" {
-  features {}
-}
-
 resource "azurerm_resource_group" "demo" {
   name     = var.az_rg_name
   location = var.az_region
